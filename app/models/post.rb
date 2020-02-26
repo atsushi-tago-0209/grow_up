e4173929cb74e9541cfe64bfc8be8b41f2db4818
atsushi-tago-0209 <0209at@gmail.com>
2020-02-26 13:38:32 +0900

    gemをインストール

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   # validates :title, :place, :belongings, :target, :sentence, :schedule, :time, presence:true
   belongs_to :user
   belongs_to :target
+  has_one :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_103711) do
+ActiveRecord::Schema.define(version: 2020_02_26_000016) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "src"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_images_on_post_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -44,4 +52,5 @@ ActiveRecord::Schema.define(version: 2020_02_23_103711) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "posts"
 end
