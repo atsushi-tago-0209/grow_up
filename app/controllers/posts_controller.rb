26e6847fbdeeddb608199c6e9bbdfcace903c5d4
atsushi-tago-0209 <0209at@gmail.com>
2020-03-07 11:22:05 +0900

    イベント作成画面　大幅に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    # binding.pry
     @posts = Post.includes(:user)
     @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(8)
   end
